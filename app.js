8743af56ce8bdde009837e1bdbc62900f376696c
Dani Prol <prolcambados@gmail.com>
2020-09-24 00:01:26 +0200

    Creating user registration

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const app = express();
 
 // Middleware:
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Response from the server");
--- routes/Auth.route.js
+++ routes/Auth.route.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const createError = require("http-errors");
+const User = require("../models/User.model");
 
 router.post("/register", async (req, res, next) => {
-  res.send("Register POST route");
+  console.log(req.body);
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) throw createError.BadRequest(); // This error will be catched by the catch block as well!
+
+    const doesExist = await User.findOne({ email: email });
+
+    if (doesExist)
+      throw createError.Conflict(`${email} has already been registered`);
+
+    const user = new User({ email, password });
+    const savedUser = await user.save();
+    res.send(savedUser); // Don't we need to send some JSON????
+  } catch (error) {
+    next(error);
+  }
+  // res.send("Register POST route");
 });
 
 router.post("/login", async (req, res, next) => {
