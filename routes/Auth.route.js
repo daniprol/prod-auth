b20d90bee555de4ea4a704af5a328be438a1da2d
Dani Prol <prolcambados@gmail.com>
2020-09-24 00:28:31 +0200

    hapi/joi schema validation

--- routes/Auth.route.js
+++ routes/Auth.route.js
@@ -2,22 +2,28 @@ const express = require("express");
 const router = express.Router();
 const createError = require("http-errors");
 const User = require("../models/User.model");
+const { authSchema } = require("../helpers/validation_schema");
 
 router.post("/register", async (req, res, next) => {
-  console.log(req.body);
   try {
     const { email, password } = req.body;
-    if (!email || !password) throw createError.BadRequest(); // This error will be catched by the catch block as well!
+    // if (!email || !password) throw createError.BadRequest(); // This error will be catched by the catch block as well!
+    const result = await authSchema.validateAsync(req.body);
+    // console.log("Joi result", result);
 
-    const doesExist = await User.findOne({ email: email });
+    // We need to pass the joi-validated data:  console.log(req.body);
+    const doesExist = await User.findOne({ email: result.email });
 
     if (doesExist)
-      throw createError.Conflict(`${email} has already been registered`);
+      throw createError.Conflict(`${result.email} has already been registered`);
 
-    const user = new User({ email, password });
+    // const user = new User({ email:result.email, password: result.password });
+    const user = new User(result);
     const savedUser = await user.save();
     res.send(savedUser); // Don't we need to send some JSON????
   } catch (error) {
+    // We need to check if the erro is coming from joi
+    if (error.isJoi === true) error.status = 422; // Otherwise it will send a 500 (interal server error) error
     next(error);
   }
   // res.send("Register POST route");
