a2c3a779230f25ada61de65afcef6c6f99575021
Dani Prol <prolcambados@gmail.com>
2020-09-24 18:21:31 +0200

    Hashing user password with mongoose middleware

--- models/User.model.js
+++ models/User.model.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const model = mongoose.model;
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema(
   {
@@ -19,6 +20,33 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+// We can't use arrow functions here so we can use the 'this' keyword
+userSchema.pre("save", async function (next) {
+  try {
+    console.log("Hashing the user password before saving to DB");
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+
+    this.password = hashedPassword;
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// We can't use arrow functions here so we can use the 'this' keyword
+// userSchema.post('save', async function(next) {
+//   try{
+//     console.log('Hashing the user password before saving to DB');
+//     const salt = await bcrypt.genSalt()
+
+//   } catch(err) {
+//     next(err)
+//   }
+
+//   next()
+// })
 
 // Always create the name of the collection in SINGULAR
 const User = model("user", userSchema);
